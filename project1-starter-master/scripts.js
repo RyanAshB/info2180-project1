bfbe712650579863d2758059dfc9fe5941f9b748
RyanAshB <ryanash.b@gmail.com>
2023-11-05 06:29:46 -0500

    Completed Project

--- project1-starter-master/scripts.js
+++ project1-starter-master/scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    const msg = document.querySelector(".message");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const userEmail = document.querySelector("#email");
+        const actEmail = userEmail.value;
+
+        if (actEmail) {
+            msg.textContent = "Thank you! Your email address " + actEmail + " has been added to our mailing list!";
+        } else {
+            msg.textContent = "Please enter a valid email address.";
+        }
+
+        userEmail.value = "";
+    });
+});
+
